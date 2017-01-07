85a6b87665488fe2b6e49b58c0fed636be14efad
Joey STANFORD <joey@stan4d.net>
2017-01-07 14:03:52 -0700

    moved hpm to new repo

--- installhpm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-npm install -g hpm-cli
-hpm install hyperterm-tab-icons
-hpm install hyperterm-paste
-hpm install hyperterm-focus-reporting
-hpm install hyperterm-cursor
-hpm instlal hyperterm-mactabs
-hpm install hyperlinks
