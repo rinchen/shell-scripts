7e78da93b97279624b7961f30bdb027e9cf67738
Joey STANFORD <rinchen@users.noreply.github.com>
2019-04-20 15:47:01 -0600

    spf13 is dead, switch to spacevim

--- installvim.sh
+++ installvim.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl http://j.mp/spf13-vim3 -L -o - | sh
+curl -sLf https://spacevim.org/install.sh | bash
--- vimupdate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/joey/.spf13-vim-3/
-git pull
-vim +BundleInstall! +BundleClean +q
